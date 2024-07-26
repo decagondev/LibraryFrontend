8c77efc294c74f91538faebff73eda6b840150ce
Tom Tarpey <tomtarpeydev@gmail.com>
2024-07-26 20:26:23 +0100

    updated be link

--- src/components/BookForm.jsx
+++ src/components/BookForm.jsx
@@ -4,54 +4,54 @@ import axios from 'axios';
 export default function BookForm() {
 
 
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [author, setAuthor] = useState('');
-    const [published, setPublished] = useState('');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [author, setAuthor] = useState('');
+  const [published, setPublished] = useState('');
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const book = { title, description, author, publishedAt: published };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const book = { title, description, author, publishedAt: published };
 
-        try {
-            const result = await axios.post("https://libapi-ett48g14.b4a.run/books", book);
-            console.log(result);
-            setTitle('');
-            setDescription('');
-            setAuthor('');
-            setPublished('');
-            alert("Successfully Create Book!");
-        } catch (err) {
-            console.error(err);
-        }
-
-        console.log("Book:", book);
+    try {
+      const result = await axios.post("https://libbe-a7p3kbc6.b4a.run/books", book);
+      console.log(result);
+      setTitle('');
+      setDescription('');
+      setAuthor('');
+      setPublished('');
+      alert("Successfully Create Book!");
+    } catch (err) {
+      console.error(err);
     }
 
-    return (
+    console.log("Book:", book);
+  }
+
+  return (
     <div className="w-full flex justify-center">
-        <div>
-            <h1 className="text-2xl">Create New Book</h1>
-            <form onSubmit={handleSubmit}>
-            <div className="flex flex-col gap-3 my-4">
-                <label className="mt-2" htmlFor="title">Title</label>
-                <input className="border p-2 rounded-md" type="text" value={title} name="title" id="title" onChange={(e) => setTitle(e.target.value)} placeholder="input your book title..." />
-            </div>
-            <div className="flex flex-col gap-3 my-4">
-                <label htmlFor="description">Description</label>
-                <textarea className="border p-2 rounded-md" rows="3"  value={description} onChange={(e) => setDescription(e.target.value)} type="text" name="description" id="description" placeholder="Add a description of your book..." />
-            </div>
-            <div className="flex flex-col gap-3 my-4">
-                <label className="mt-2" htmlFor="published">Published</label>
-                <input className="border p-2 rounded-md" value={published} onChange={(e) => setPublished(e.target.value)} type="date" name="published" id="published" />
-            </div>
-            <div className="flex flex-col gap-3 my-4">
-                <label className="mt-2" htmlFor="author">Author</label>
-                <input className="border p-2 rounded-md" value={author} onChange={(e) => setAuthor(e.target.value)} type="text" name="author" id="author" placeholder="input the books author..." />
-            </div>
-            <button  className="border p-2 rounded-md w-full bg-blue-400 text-white hover:text-black hover:bg-blue-300" type="submit">Save</button>
-            </form>
-        </div>
+      <div>
+        <h1 className="text-2xl">Create New Book</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-3 my-4">
+            <label className="mt-2" htmlFor="title">Title</label>
+            <input className="border p-2 rounded-md" type="text" value={title} name="title" id="title" onChange={(e) => setTitle(e.target.value)} placeholder="input your book title..." />
+          </div>
+          <div className="flex flex-col gap-3 my-4">
+            <label htmlFor="description">Description</label>
+            <textarea className="border p-2 rounded-md" rows="3" value={description} onChange={(e) => setDescription(e.target.value)} type="text" name="description" id="description" placeholder="Add a description of your book..." />
+          </div>
+          <div className="flex flex-col gap-3 my-4">
+            <label className="mt-2" htmlFor="published">Published</label>
+            <input className="border p-2 rounded-md" value={published} onChange={(e) => setPublished(e.target.value)} type="date" name="published" id="published" />
+          </div>
+          <div className="flex flex-col gap-3 my-4">
+            <label className="mt-2" htmlFor="author">Author</label>
+            <input className="border p-2 rounded-md" value={author} onChange={(e) => setAuthor(e.target.value)} type="text" name="author" id="author" placeholder="input the books author..." />
+          </div>
+          <button className="border p-2 rounded-md w-full bg-blue-400 text-white hover:text-black hover:bg-blue-300" type="submit">Save</button>
+        </form>
+      </div>
     </div>
-    );
+  );
 }
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,41 +3,41 @@ import axios from 'axios';
 import { useEffect, useState } from "react";
 
 export default function BookList() {
-    const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState([]);
 
-    useEffect(() => {
-        getBooks();
-    }, [])
+  useEffect(() => {
+    getBooks();
+  }, [])
 
-    const getBooks = async () => {
-        try {
-            const result = await axios.get('https://libapi-ett48g14.b4a.run/books');
-            console.log(result);
-            setBooks(result.data);
-        } catch (err) {
-            console.log(err);
-        }
-    } 
+  const getBooks = async () => {
+    try {
+      const result = await axios.get('https://libbe-a7p3kbc6.b4a.run/books');
+      console.log(result);
+      setBooks(result.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-    return (
-        <div className="w-full flex justify-center">
-            <div className="flex flex-col text-center w-[97%]">
+  return (
+    <div className="w-full flex justify-center">
+      <div className="flex flex-col text-center w-[97%]">
 
-            <h1 className="text-2xl">Books</h1>
-                <ul className="flex flex-col gap-4 border rounded-md shadow-lg p-5 m-4 w-[80%] self-center">
-                    {
-                        books.map(book => (
-                            <li className="border rounded-lg p-6" key={book.id}>
-                                <Link to={`/books/${book.id}`}>
-                                    <h2 className="text-xl">{book.title}</h2>
-                                    <h3 className="text-lg">{book.author}</h3>
-                                    <p className="font-bold">{book.publishedAt}</p>
-                                </Link>
-                            </li>
-                        ))
-                    }
-                </ul>
-            </div>
-        </div>
-    );
+        <h1 className="text-2xl">Books</h1>
+        <ul className="flex flex-col gap-4 border rounded-md shadow-lg p-5 m-4 w-[80%] self-center">
+          {
+            books.map(book => (
+              <li className="border rounded-lg p-6" key={book.id}>
+                <Link to={`/books/${book.id}`}>
+                  <h2 className="text-xl">{book.title}</h2>
+                  <h3 className="text-lg">{book.author}</h3>
+                  <p className="font-bold">{book.publishedAt}</p>
+                </Link>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </div>
+  );
 }
